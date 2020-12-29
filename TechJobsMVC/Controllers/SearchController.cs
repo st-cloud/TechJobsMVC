1a0789623ca1c2355db5311021c742dda7768d74
Grace Brown <gracenbrown@sbcglobal.net>
2020-12-29 14:07:56 -0600

    Completed Search functionality (tasks 3 and 4)

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Models;
+using TechJobsMVC.Data;
 
 namespace TechJobsMVC.Controllers
 {
@@ -15,6 +17,26 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+
+            if (searchTerm == null || searchTerm == "")
+            {
+                jobs = JobData.FindAll();
+                //ViewBag.title = "All Jobs";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                //ViewBag.title = "Jobs with " + ListController.ColumnChoices[searchType] + ": " + searchTerm;
+
+            }
+
+            ViewBag.jobs = jobs;
+
+            return View("Index");
+        }
     }
 }
